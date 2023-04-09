7c75d64ea3b16239df13c6135991910908c3001d
Gantulga Enkhbat <122564642+Gantulga202@users.noreply.github.com>
2023-04-08 22:59:08 -0400

    Create 7kyu_task1.js
    Disemvowel Trolls

---
+++ 7kyu_task1.js
@@ -0,0 +1,29 @@
+/* 
+Gantulga Enkhbat - CodeWars "7Kyu solutions" 
+
+Format:
+1. Challenge ( KATA )
+2. Solutions
+
+Task: Disemvowel Trolls
+
+1. Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+*/
+2. 
+function disemvowel(str) {
+    return str.replaceAll( /[aeiouAEIOU]/g, "" )
+    
+  }
+  
+  
+  
+  
+  
