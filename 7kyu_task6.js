7c26cb20f05f337b6ee6108a2e3b186e11bd972d
Gantulga Enkhbat <122564642+Gantulga202@users.noreply.github.com>
2023-04-10 22:17:53 -0400

    Create 7kyu_task6.js
    Reverse words

---
+++ 7kyu_task6.js
@@ -0,0 +1,25 @@
+/* 
+Gantulga Enkhbat - CodeWars "7Kyu solutions" 
+
+Format:
+1. Challenge ( KATA )
+2. Solutions
+
+Task: Reverse words
+
+1. Complete the function that accepts a string parameter, and reverses each word in the string. 
+All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+*/
+2. 
+function reverseWords(str) {
+    let arr = []
+    let nStr = str.split(' ')
+    for(let i = 0; i < nStr.length; i++){
+      arr.push(nStr[i].split('').reverse().join(''))
+    }
+    return arr.join(' ')
+    }
