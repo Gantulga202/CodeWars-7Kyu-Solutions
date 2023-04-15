45e2afe85aa98e582fa685fde4643db687704a4b
Gantulga Enkhbat <122564642+Gantulga202@users.noreply.github.com>
2023-04-14 22:54:35 -0400

    Create 7kyu_task8.js
    Friend or Foe?

---
+++ 7kyu_task8.js
@@ -0,0 +1,28 @@
+/* 
+Gantulga Enkhbat - CodeWars "7Kyu solutions" 
+
+Format:
+1. Challenge ( KATA )
+2. Solutions
+
+Task: Friend or Foe?
+
+
+1. DESCRIPTION:
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! 
+Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e.
+
+friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+Note: keep the original order of the names in the output.
+*/
+
+2. 
+function friend(friends){
+    return friends.filter( item => item.length == 4 )
+  }
