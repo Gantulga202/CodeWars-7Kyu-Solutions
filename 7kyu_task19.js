90ea99b0c4e819546babd883fd64c4c3416da996
Gantulga Enkhbat <122564642+Gantulga202@users.noreply.github.com>
2023-04-22 15:46:57 -0400

    Create 7kyu_task19.js
    Odd or Even?

---
+++ 7kyu_task19.js
@@ -0,0 +1,27 @@
+/* 
+Gantulga Enkhbat - CodeWars "7Kyu solutions" 
+
+Format:
+1. Challenge ( KATA )
+2. Solutions
+
+Task: Odd or Even?
+
+1. Task:
+Given a list of integers, determine whether the sum of its elements is odd or even.
+Give your answer as a string matching "odd" or "even".
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+Input: [0, 1, 4]
+Output: "odd"
+Input: [0, -1, -5]
+Output: "even"
+*/
+
+2. 
+function oddOrEven(array) {
+    return array.reduce( (acc,c)=> acc+c,0 ) % 2 === 0 ? 'even' : 'odd'
+ }
